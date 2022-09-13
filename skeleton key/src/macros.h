596f1115362f4a541e08897e47b380235e222f53
classic-sasquatch-behavior <classic.sasquatch.behavior@gmail.com>
2022-09-12 21:18:31 -0400

    made major changes to the FOR loops. next is to merge this branch and to write the documentation.

--- skeleton key/src/macros.h
+++ skeleton key/src/macros.h
@@ -14,6 +14,7 @@
 		int & _FIRST_ = _first_dim_;\
 		int & _SECOND_ = _second_dim_;
 
+
 	//establish the coordinates of the current thread. this one is for 3d kernels		
 	#define DIMS_3D(_first_dim_, _second_dim_, _third_dim_)\
 		int _first_dim_ = (blockIdx.x * blockDim.x) + threadIdx.x; \
@@ -25,19 +26,26 @@
 
 #pragma endregion
 
+
 #pragma region bounds
 
 	//checks that the thread falls within the bounds of the problem. this one is for 1d kernels
 	#define BOUNDS_1D(_first_span_)\
-		if((_FIRST_ < 0)||(_FIRST_ >= _first_span_)){return;} 
+		const int& _FIRST_SPAN_ = _first_span_;\
+		if((_FIRST_ < 0)||(_FIRST_ >= _FIRST_SPAN_)){return;} 
 
 	//checks that the thread falls within the bounds of the problem. this one is for 2d kernels
 	#define BOUNDS_2D(_first_span_, _second_span_)\
-		if((_FIRST_ < 0)||(_SECOND_ < 0)||(_FIRST_ >= _first_span_)||(_SECOND_ >= _second_span_)){return;} 
+		const int& _FIRST_SPAN_ = _first_span_;\
+		const int& _SECOND_SPAN_ = _second_span_;\
+		if((_FIRST_ < 0)||(_SECOND_ < 0)||(_FIRST_ >= _FIRST_SPAN_)||(_SECOND_ >= _SECOND_SPAN_)){return;} 
 
 	//checks that the thread falls within the bounds of the problem. this one is for 3d kernels
 	#define BOUNDS_3D(_first_span_, _second_span_, _third_span_)\
-		if((_FIRST_ < 0)||(_SECOND_ < 0)||(_THIRD_ < 0)||(_FIRST_ >= _first_span_)||(_SECOND_ >= _second_span_)||(_THIRD_ >= _third_span_)){return;} 
+		const int& _FIRST_SPAN_ = _first_span_;\
+		const int& _SECOND_SPAN_ = _second_span_;\
+		const int& _THIRD_SPAN_ = _third_span_;\
+		if((_FIRST_ < 0)||(_SECOND_ < 0)||(_THIRD_ < 0)||(_FIRST_ >= _FIRST_SPAN_)||(_SECOND_ >= _SECOND_SPAN_)||(_THIRD_ >= _THIRD_SPAN_)){return;} 
 
 #pragma endregion
 
@@ -61,36 +69,38 @@
 
 #pragma region spatial operations
 
-	#define FOR_MXN_INCLUSIVE(_new_maj_, _new_min_, _M_, _N_, _maj_max_, _min_max_, _origin_maj_, _origin_min_, ...)				 \
-	int _maj_limit_ = (_M_ - (_M_ % 2)) / 2;																			   			 \
-	int _min_limit_ = (_N_ - (_N_ % 2)) / 2;																						 \
-		__pragma(unroll) for (int _neighbor_maj_ = -_maj_limit_; _neighbor_maj_ < (_maj_limit_ + (_M_ % 2)); _neighbor_maj_++) {	 \
-			__pragma(unroll) for (int _neighbor_min_ = -_min_limit_; _neighbor_min_ < (_min_limit_ + (_N_ % 2)); _neighbor_min_++) { \
-				int _new_maj_ = _origin_maj_ + _neighbor_maj_;																		 \
-				int _new_min_ = _origin_min_ + _neighbor_min_;																	     \
-				if((_new_maj_ < 0)||(_new_min_ < 0)||(_new_maj_ >= _maj_max_)||(_new_min_ >= _min_max_ )){continue;}				 \
-				__VA_ARGS__;																										 \
-			}																														 \
+	#define FOR_MXN_INCLUSIVE(_new_first_, _new_second_, _M_, _N_, ...)																  \
+	int _first_limit_ = (_M_ - (_M_ % 2)) / 2;																			   			  \
+	int _second_limit_ = (_N_ - (_N_ % 2)) / 2;																						  \
+		__pragma(unroll) for (int _n_first_ = -_first_limit_; _n_first_ < (_first_limit_ + (_M_ % 2)); _n_first_++) {				  \
+			__pragma(unroll) for (int _n_second_ = -_second_limit_; _n_second_ < (_second_limit_ + (_N_ % 2)); _n_second_++) {		  \
+				int _new_first_ = _FIRST_ + _n_first_;																				  \
+				int _new_second_ = _SECOND_ + _n_second_;																			  \
+				if((_new_first_ < 0)||(_new_second_ < 0)||(_new_first_ >= _FIRST_SPAN_)||(_new_second_ >= _SECOND_SPAN_ )){continue;} \
+				__VA_ARGS__;																										  \
+			}																														  \
 		}
 
-	#define FOR_MXN_EXCLUSIVE(_new_maj_, _new_min_, _M_, _N_, _maj_max_, _min_max_, _origin_maj_, _origin_min_, ...)				 \
-	int _maj_limit_ = (_M_ - (_M_ % 2)) / 2;																						 \
-	int _min_limit_ = (_N_ - (_N_ % 2)) / 2;																						 \
-		__pragma(unroll) for (int _neighbor_maj_ = -_maj_limit_; _neighbor_maj_ < (_maj_limit_ + (_M_ % 2)); _neighbor_maj_++) {	 \
-			__pragma(unroll) for (int _neighbor_min_ = -_min_limit_; _neighbor_min_ < (_min_limit_ + (_N_ % 2)); _neighbor_min_++) { \
-				int _new_maj_ = _origin_maj_ + _neighbor_maj_;																		 \
-				int _new_min_ = _origin_min_ + _neighbor_min_;																		 \
-				if((_new_maj_ < 0)||(_new_min_ < 0)||(_new_maj_ >= _maj_max_)||(_new_min_ >= _min_max_ )							 \
-								  ||((_new_maj_ == _origin_maj_)&&(_new_min_ == _origin_min_))) {continue;}							 \
-				__VA_ARGS__;																										 \
-			}																														 \
+
+		//todo: implement contextual FIRST and SECOND objects
+	#define FOR_MXN_EXCLUSIVE(_new_first_, _new_second_, _M_, _N_, ...)														   \
+	int _first_limit_ = (_M_ - (_M_ % 2)) / 2;																			   	   \
+	int _second_limit_ = (_N_ - (_N_ % 2)) / 2;																				   \
+		__pragma(unroll) for (int _n_first_ = -_first_limit_; _n_first_ < (_first_limit_ + (_M_ % 2)); _n_first_++) {		   \
+			__pragma(unroll) for (int _n_second_ = -_second_limit_; _n_second_ < (_second_limit_ + (_N_ % 2)); _n_second_++) { \
+				int _new_first_ = _FIRST_ + _n_first_;																		   \
+				int _new_second_ = _SECOND_ + _n_second_;																	   \
+				if((_new_first_ < 0)||(_new_second_ < 0)||(_new_first_ >= _FIRST_SPAN_)||(_new_second_ >= _SECOND_SPAN_ )	   \
+								  ||((_new_first_ == _FIRST_)&&(_new_second_ == _SECOND_))) {continue;}						   \
+				__VA_ARGS__;																								   \
+			}																												   \
 		}
 
-	#define FOR_3X3_INCLUSIVE(_new_maj_, _new_min_, _maj_max_, _min_max_, _origin_maj_, _origin_min_, ...) \
-		FOR_MXN_INCLUSIVE(_new_maj_, _new_min_, 3, 3, _maj_max_, _min_max_, _origin_maj_, _origin_min_, __VA_ARGS__)
+	#define FOR_3X3_INCLUSIVE(_new_maj_, _new_min_, ...) \
+		FOR_MXN_INCLUSIVE(_new_maj_, _new_min_, 3, 3, __VA_ARGS__)
 
-	#define FOR_NEIGHBOR(_new_maj_, _new_min_, _maj_max_, _min_max_, _origin_maj_, _origin_min_, ...) \
-		FOR_MXN_EXCLUSIVE(_new_maj_, _new_min_, 3, 3, _maj_max_, _min_max_, _origin_maj_, _origin_min_, __VA_ARGS__)
+	#define FOR_NEIGHBOR(_new_maj_, _new_min_, ...) \
+		FOR_MXN_EXCLUSIVE(_new_maj_, _new_min_, 3, 3, __VA_ARGS__)
 
 #pragma endregion
 
@@ -115,6 +125,8 @@
 		__pragma(unroll) for (int i = 0; i < _length_; i++){\
 			_to_[i] = _from_[i];\
 		}
+
+	#define SELF _FIRST_, _SECOND_
 
  #pragma endregion
 
